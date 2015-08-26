4e113c32f81dfbcfee84457832305a94f49c22ae
eminyuce <eminyuce@gmail.com>
2015-08-26 16:34:43 -0400

    Adding overloaded constructors

--- src/GenericRepository.EntityFramework/EntitiesContext.cs
+++ src/GenericRepository.EntityFramework/EntitiesContext.cs
@@ -13,18 +13,105 @@ namespace GenericRepository.EntityFramework {
     public abstract class EntitiesContext : DbContext, IEntitiesContext
     {
 
-     
+        /// <summary>
+        /// Constructs a new context instance using conventions to create the name of
+        /// the database to which a connection will be made. The by-convention name is
+        /// the full name (namespace + class name) of the derived context class.  See
+        /// the class remarks for how this is used to create a connection. 
+        /// </summary>
+        protected EntitiesContext()
+            : base()
+        {
+        }
+
+        /// <summary>
+        /// Constructs a new context instance using conventions to create the name of
+        /// the database to which a connection will be made, and initializes it from
+        /// the given model.  The by-convention name is the full name (namespace + class
+        /// name) of the derived context class.  See the class remarks for how this is
+        /// used to create a connection.
+        /// </summary>
+        /// <param name="model">The model that will back this context.</param>
+        protected EntitiesContext(DbCompiledModel model)
+            : base(model)
+        {
+        }
+
         /// <summary>
         /// Constructs a new context instance using the given string as the name or connection
         /// string for the database to which a connection will be made.  See the class
         /// remarks for how this is used to create a connection.
         /// </summary>
         /// <param name="nameOrConnectionString">Either the database name or a connection string.</param>
-        protected EntitiesContext(string nameOrConnectionString) : base(nameOrConnectionString) { 
-
-         
+        public EntitiesContext(string nameOrConnectionString)
+            : base(nameOrConnectionString)
+        {
         }
-         
+
+        /// <summary>
+        /// Constructs a new context instance using the existing connection to connect
+        /// to a database.  The connection will not be disposed when the context is disposed.
+        /// </summary>
+        /// <param name="existingConnection">An existing connection to use for the new context.</param>
+        /// <param name="contextOwnsConnection">
+        /// If set to true the connection is disposed when the context is disposed, otherwise
+        /// the caller must dispose the connection.
+        /// </param>
+        public EntitiesContext(DbConnection existingConnection, bool contextOwnsConnection)
+            : base(existingConnection, contextOwnsConnection)
+        {
+        }
+
+        /// <summary>
+        /// Constructs a new context instance around an existing ObjectContext.  An existing
+        /// ObjectContext to wrap with the new context.  If set to true the ObjectContext
+        /// is disposed when the EntitiesContext is disposed, otherwise the caller must dispose
+        /// the connection.
+        /// </summary>
+        /// <param name="objectContext">An existing ObjectContext to wrap with the new context.</param>
+        /// <param name="EntitiesContextOwnsObjectContext">
+        /// If set to true the ObjectContext is disposed when the EntitiesContext is disposed,
+        /// otherwise the caller must dispose the connection.
+        /// </param>
+        public EntitiesContext(ObjectContext objectContext, bool EntitiesContextOwnsObjectContext)
+            : base(objectContext, EntitiesContextOwnsObjectContext)
+        {
+        }
+
+        /// <summary>
+        /// Constructs a new context instance using the given string as the name or connection
+        /// string for the database to which a connection will be made, and initializes
+        /// it from the given model.  See the class remarks for how this is used to create
+        /// a connection.
+        /// </summary>
+        /// <param name="nameOrConnectionString">Either the database name or a connection string.</param>
+        /// <param name="model">The model that will back this context.</param>
+        public EntitiesContext(string nameOrConnectionString, DbCompiledModel model)
+            : base(nameOrConnectionString, model)
+        {
+        }
+
+        /// <summary>
+        /// Constructs a new context instance using the existing connection to connect
+        /// to a database, and initializes it from the given model.  The connection will
+        /// not be disposed when the context is disposed.  An existing connection to
+        /// use for the new context.  The model that will back this context.  If set
+        /// to true the connection is disposed when the context is disposed, otherwise
+        /// the caller must dispose the connection.
+        /// </summary>
+        /// <param name="existingConnection">An existing connection to use for the new context.</param>
+        /// <param name="model">The model that will back this context.</param>
+        /// <param name="contextOwnsConnection">
+        /// If set to true the connection is disposed when the context is disposed, otherwise
+        /// the caller must dispose the connection.
+        /// </param>
+        public EntitiesContext(DbConnection existingConnection, DbCompiledModel model, bool contextOwnsConnection)
+            : base(existingConnection, model, contextOwnsConnection)
+        {
+        }
+
+
+
         /// <summary>
         /// Returns a DbSet instance for access to entities of the given type in the context.
         /// </summary>
